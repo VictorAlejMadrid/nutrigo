0d5b8d1795b313a26bb29f40a6f7e412e8a0f91d
VictorAlejMadrid <victoralej.madrid@gmail.com>
2026-06-22 20:46:58 -0300

    fix: build and npm audit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['172.20.10.5'],
+  basePath: process.env.NODE_ENV === 'production' ? '/nutrigo' : '',
 };
 
 export default nextConfig;
